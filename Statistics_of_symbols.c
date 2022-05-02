26d2042c9e6e0ef71d23d4fb731e734343f53199
Dmitry <dima.namyotkin@yandex.ru>
2022-05-02 21:01:04 +0300

    bugs fixed

--- Statistics_of_symbols.c
+++ Statistics_of_symbols.c
@@ -9,10 +9,11 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-char **getSymbolsFromKeyboard(int **arr_size) // Получение символов с клавиатуры и запись в массив arr
+int getSymbolsFromKeyboard(char ***arr,int **arr_size) // Получение символов с клавиатуры и запись в массив arr
 {
-  char **arr;
-  arr = (char**)malloc(sizeof(char**));
+  char **arr1;
+  **arr_size = 0;
+  arr1 = (char**)malloc(sizeof(char*));
   char char_choice; // Символ вводимый пользователем
 
   printf("Введите точку '.', чтобы окончить ввод новых символов\n");
@@ -24,19 +25,25 @@ char **getSymbolsFromKeyboard(int **arr_size) // Получение символ
       
       if(char_choice == '.') break;
 
-      *arr = (char*)realloc(*arr, **arr_size + 1);
-      arr[0][arr_size[0][0]++] = char_choice;
+      *arr1 = (char*)realloc(*arr1, **arr_size + 1);
+      arr1[0][arr_size[0][0]++] = char_choice;
      }
   printf("\n");
 
-  return arr;
+    if (*arr1 == NULL)
+    {
+      free(*arr);
+      *arr = NULL;
+      }
+  *arr = arr1;
+  return 1;
 }
 
 
-char **getSymbolsFromFile(int **arr_size) // Получение символов из файла и запись в массив arr1
+int getSymbolsFromFile(char ***arr, int **arr_size) // Получение символов из файла и запись в массив arr1
 {
   char some_char;
-  char **arr = (char**)malloc(sizeof(char**));;
+   *arr = (char**)malloc(sizeof(char*));
   **arr_size = 0;
   char name_file[31];
 
@@ -44,19 +51,25 @@ char **getSymbolsFromFile(int **arr_size) // Получение символов
   scanf("%30s", name_file);
 
   FILE *file = fopen(name_file, "r");
-  if (file == NULL) return 0;
-  
+  if (file == NULL)
+    {
+      free(*arr);
+      *arr = NULL;
+      printf("Ошибка: Не удалось открыть файл!\n");
+      return 0;
+    }
+
   while (fgetc(file) != EOF)
     {
       ++**arr_size;
     }
   
   rewind(file);
-  *arr = malloc(sizeof(char) * **arr_size - 1);
+  **arr = malloc(sizeof(char) * **arr_size - 1);
   
   for (int i = 0; (some_char = fgetc(file)) != EOF;)
     {
-      arr[0][i] = some_char;
+      arr[0][0][i] = some_char;
       ++i;
     }
 
@@ -65,13 +78,21 @@ char **getSymbolsFromFile(int **arr_size) // Получение символов
   --**arr_size;
   printf("\n");
  
-  return arr;
+  return 1;
 }
 
 
-char **sortingSymbols(char **arr1, int **arr_size1, int **all_char, int **max_number_char) // Сортировка и деление на элементы массива arr1
+int sortingSymbols(char ***arr1, int **arr_size1, int **all_char, int **max_number_char) // Сортировка и деление на элементы массива arr1
 {                                                                                          // с записью в массив arr2
+  if (*arr1 == NULL)
+    {
+      printf("Ошибка: Нет элементов для сортировки!\n");
+      return 0;
+    }
+  
   char **arr2 = NULL;
+  arr2 = (char**)malloc(sizeof(char*));
+
   int arr_size2 = 0;
 
   **all_char = 0; // Количество одиночных символов(ширина таблицы)
@@ -80,28 +101,29 @@ char **sortingSymbols(char **arr1, int **arr_size1, int **all_char, int **max_nu
 
   for (int i = 0; i < **arr_size1; ++i)
     {
-      if (arr1[0][i] == ' ') continue;
+      if (arr1[0][0][i] == ' ') continue;
 
       int many_char = 0;
       int sector_size = 0;
 
-      temp = arr1[0][i];
+      temp = arr1[0][0][i];
       ++**all_char;
       ++**all_char;
-      arr2 = (char**)realloc(arr2, sizeof(char**) * **all_char);
+      arr2 = (char**)realloc(arr2, sizeof(char*) * (**all_char));
       arr2[arr_size2] = (char*)malloc(sizeof(char));
 
       for(int j = 0; j < **arr_size1; ++j)
 	{
-	  if (arr1[0][j] == temp)
+	  if (arr1[0][0][j] == temp)
 	    {
 	      arr2[arr_size2] = (char*)realloc(arr2[arr_size2], sizeof(char) * (sector_size + 1));
-	      arr2[arr_size2][sector_size++] = arr1[0][j];
-	      arr1[0][j] = ' ';
+	      arr2[arr_size2][sector_size++] = arr1[0][0][j];
+	      arr1[0][0][j] = ' ';
 	      ++many_char;
 	    }
 	  else continue;
 	}
+
       arr2[arr_size2] = (char*)realloc(arr2[arr_size2], sizeof(char) * sector_size);
       arr2[arr_size2][sector_size] = '0';
       ++arr_size2;
@@ -116,18 +138,27 @@ char **sortingSymbols(char **arr1, int **arr_size1, int **all_char, int **max_nu
   printf("Количество символов: %d\n", **all_char / 2);
   printf("Самая большая величина: %d\n", **max_number_char);
 
-  free(*arr1);
-  arr1 = NULL;
-  return arr2;
+  free(**arr1);
+  **arr_size1 = 0;
+  *arr1 = arr2;
+  arr2 = NULL;
+  
+  return 1;
 }
 
 
-char **createTable(char ***arr2, int **all_char, int **max_number_char, int view) // Отрисовка таблицы в arr1 с переносом элементов из arr2
+int createTable(char ***arr2, int **all_char, int **max_number_char, int view) // Отрисовка таблицы в arr1 с переносом элементов из arr2
 {
+  if (*arr2 == NULL)
+    {
+      printf("Ошибка: Нет элементов для создания таблицы!\n");
+      return 0;
+    }
+  
   char **arr1 = NULL;
   
   int var_switch = 0; // Переключатель сигнализирующий что символы закончились
-  all_char[0][0] += 2;        // Прибавление 2 символа к ширине для отрисовки таблицы
+  all_char[0][0] += 3;        // Прибавление 2 символа к ширине для отрисовки таблицы
   max_number_char[0][0] += 2; // Прибавление 2 символа к высоте для отрисовки таблицы
   
   arr1 = (char**)calloc(**all_char, sizeof(char*)); // Выделение памяти под ширину таблицы
@@ -139,21 +170,20 @@ char **createTable(char ***arr2, int **all_char, int **max_number_char, int view
     {
       for(int j = 0; j < **max_number_char; ++j)
 	{
-       	   if (i == 0 && j > 1)
-	     arr1[i][j] = (j - 1) + '0';
+	  if (i < 2 && j > 1) {if (j < 11) arr1[i][j] = ' '; else continue;}
 
-	   else  if (j == 0 && i > 1)
+	   else  if (j == 0 && i > 2)
 	     {
-	       if(arr2[0][i-2][0] == '0')
+	       if(arr2[0][i-3][0] == '0')
 		 arr1[i][j] = ' ';
 	       else
-		 arr1[i][j] = arr2[0][i-2][0];
+		 arr1[i][j] = arr2[0][i-3][0];
 	     }
 	   
-	  else if(j == 1 && i == 1)
+	  else if(j == 1 && i == 2)
 	    arr1[i][j] = '+';
 
-	  else if (i == 1)
+	  else if (i == 2)
 	    arr1[i][j] = '|';
 
 	  else if (j == 1)
@@ -165,13 +195,13 @@ char **createTable(char ***arr2, int **all_char, int **max_number_char, int view
     }
 
   
-  for (int i = 2; i < **all_char; ++i) // Отрисовка символов в таблице
+  for (int i = 3; i < **all_char; ++i) // Отрисовка символов в таблице
     {
       var_switch = 0;
 
       for (int j = 2; j < **max_number_char; ++j)
 	{
-	  if (var_switch == 1 || arr2[0][i-2][j-2] == '0')
+	  if (var_switch == 1 || arr2[0][i-3][j-2] == '0')
 	    {
 	      var_switch = 1;
 	      arr1[i][j] = ' ';
@@ -179,35 +209,61 @@ char **createTable(char ***arr2, int **all_char, int **max_number_char, int view
 
 	  else
 	    {
-	      if (view == 1) arr1[i][j] = arr2[0][i-2][j-2];
+	      if (view == 1) arr1[i][j] = arr2[0][i-3][j-2];
 	      else if (view == 2) arr1[i][j] = '+';
 	    }
 	  
 	}
     }
 
-  for (int i = 0; i < **all_char - 2; ++i)
+  for (int i = 0; i < **all_char - 3; ++i)
     free(arr2[0][i]);
-
-  free(arr2[0]);
-  arr2 = NULL;
+  
+  free(*arr2);
+  *arr2 = arr1;
+  arr1 = NULL;
     
-  return arr1;
+  return 1;
 }
 
 
-void drawTable(char ***arr1, int **all_char, int **max_number_char) // Отрисовка таблицы на экране
+int drawTable(char ***arr1, int **all_char, int **max_number_char) // Отрисовка таблицы на экране
 {
+  if (*arr1 == NULL) {printf("Ошибка: Нет элементов для отображения!\n"); return 0;}
   for(int j = **max_number_char-1; j >= 0; --j)
     {
       printf("\n");
       for(int i = 0; i < **all_char; ++i)
 	{
-	  printf("%c", arr1[0][i][j]);
+	  if (i == 0 && j > 1) printf("%d", (j - 1));
+	  else printf("%c", arr1[0][i][j]);
 	}     
     }
   
   printf("\n");
+
+  return 1;
+}
+
+
+int deleteTable(char ***arr, int **width_Table, int **height_Table)
+{
+  if (*arr == NULL)
+    {
+      printf("Ошибка: Нет элементов для удаления!");
+      return 0;
+    }
+
+    for (int i = 0; i < **width_Table; ++i)
+      {
+	free(arr[0][i]);
+      }
+    free(*arr);
+    *arr = NULL;
+    **width_Table = 0;
+    **height_Table = 0;
+    
+  return 1;
 }
 
 
@@ -255,20 +311,15 @@ int main()
 		{
 		  var2 = -1;
 		  
-		  arr = getSymbolsFromKeyboard(&arr_size);
-		  arr = sortingSymbols(&(*arr), &arr_size, &width_Table, &height_Table);
-		  arr = createTable(&arr, &width_Table, &height_Table, view);
+		  getSymbolsFromKeyboard(&arr, &arr_size);
+		  if (arr != NULL)
+		    {
+		  sortingSymbols(&arr, &arr_size, &width_Table, &height_Table);
+		  createTable(&arr, &width_Table, &height_Table, view);
 		  drawTable(&arr, &width_Table, &height_Table);
-		  
-		  for (int i = 0; i < *width_Table - 2; ++i)
-		    free(arr[i]);
-		  
-		  free(arr);
-		  arr = NULL;
-		  *arr_size = 0;
-		  *width_Table = 0;
-		  *height_Table = 0;
-		  
+		  deleteTable(&arr, &width_Table, &height_Table);
+		    }
+		 
 		  printf("\nМеню программы.\n1.Ввод данных\n2.Настройка отображения символов\n0.Выход из программы\n");
 		  break;
 		}
@@ -277,19 +328,14 @@ int main()
 		{
 		  var2 = -1;
 		  
-		  arr = getSymbolsFromFile(&arr_size);
-		  arr = sortingSymbols(&(*arr), &arr_size, &width_Table, &height_Table);
-		  arr = createTable(&arr, &width_Table, &height_Table, view);
+		  getSymbolsFromFile(&arr, &arr_size);
+		  if (arr != NULL)
+		    {
+		  sortingSymbols(&arr, &arr_size, &width_Table, &height_Table);
+		  createTable(&arr, &width_Table, &height_Table, view);
 		  drawTable(&arr, &width_Table, &height_Table);
-		  
-		  for (int i = 0; i < *width_Table - 2; ++i)
-		    free(arr[i]);
-		  
-		  free(arr);
-		  arr = NULL;
-		  *arr_size = 0;
-		  *width_Table = 0;
-		  *height_Table = 0;
+		  deleteTable(&arr, &width_Table, &height_Table);
+		    }
 		  
 		  printf("\nМеню программы.\n1.Ввод данных\n2.Настройка отображения символов\n0.Выход из программы\n");
 		  break;
